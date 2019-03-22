dc3d6f3058264756abb7e17de4c0ac3e78a236b0
Wolf480pl <wolf480@interia.pl>
2019-03-22 16:01:36 +0100

    Add some files whose disasm may be of interest

---
+++ tmp.c
@@ -0,0 +1,3 @@
+long long foo(long long a, int x, int y) {
+    return a + x * y;
+}
---
+++ tmp1.asm
@@ -0,0 +1,90 @@
+section .text
+
+
+load_edi:
+    nop
+    nop
+    nop
+    nop
+    mov edi, [ebp+0x55]
+
+load_rdi:
+    nop
+    nop
+    nop
+    mov rdi, [ebp+0x55]
+
+load_esi:
+    nop
+    nop
+    nop
+    nop
+    mov esi, [ebp+0x55]
+
+load_rsi:
+    nop
+    nop
+    nop
+    mov rsi, [ebp+0x55]
+
+load_edx:
+    nop
+    nop
+    nop
+    nop
+    mov edx, [ebp+0x55]
+
+load_rdx:
+    nop
+    nop
+    nop
+    mov rdx, [ebp+0x55]
+
+load_ecx:
+    nop
+    nop
+    nop
+    nop
+    mov ecx, [ebp+0x55]
+
+load_rcx:
+    nop
+    nop
+    nop
+    mov rcx, [ebp+0x55]
+
+load_r8:
+    nop
+    nop
+    nop
+    mov r8,  [ebp+0x55]
+
+load_r8d:
+    nop
+    nop
+    nop
+    mov r8d, [ebp+0x55]
+
+load_r9:
+    nop
+    nop
+    nop
+    mov r9,  [ebp+0x55]
+
+load_r9d:
+    nop
+    nop
+    nop
+    mov r9d, [ebp+0x55]
+
+push32_A:
+    push word [ebp+0x57]
+    push word [ebp+0x55]
+
+push32_B:
+    sub esp, 4
+    mov r10d, [ebp+0x55]
+    mov [esp], r10d
+
+push64:
+    push qword [ebp+0x55]
