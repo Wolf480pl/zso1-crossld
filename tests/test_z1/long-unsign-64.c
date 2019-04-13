1505415c6dff7fe657bd513f1d7a2bc16ca9cfe3
Wolf480pl <wolf480@interia.pl>
2019-04-13 11:07:37 +0200

    Add tests for returning signed and unsigned longs

---
+++ tests/test_z1/long-unsign-64.c
@@ -0,0 +1,45 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "crossld.h"
+
+static unsigned long get_long64() {
+	return (unsigned long) (1ULL<<62);
+}
+
+static unsigned long get_long32() {
+	return (unsigned long) 0xFFFFFFFF;
+}
+
+static unsigned long get_long_negative() {
+	return (unsigned long) -1;
+}
+
+static void test(struct function *func, bool shouldSucceed, char *desc) {
+	int v;
+	v = crossld_start("long-ptr-32", func, 1);
+	if (v != -1 && v != 0) {
+		printf("Invalid response: %d\n", v);
+		exit(-1);
+	}
+    if ((v == 0) == shouldSucceed) {
+		printf("step OK\n");
+		return;
+	} else {
+		printf("Invalid response: %d\n", v);
+		exit(-1);
+	}
+}
+
+int main() {
+	struct function funcs[] = {
+		{"get_ptr64", 0, 0, TYPE_UNSIGNED_LONG, get_long64},
+		{"get_ptr64", 0, 0, TYPE_UNSIGNED_LONG, get_long32},
+		{"get_ptr64", 0, 0, TYPE_UNSIGNED_LONG, get_long_negative},
+	};
+	
+	test(&funcs[0], false, "get_long64");
+	test(&funcs[1], true, "get_long32");
+	test(&funcs[2], false, "get_long_negative");
+	printf("OK\n");
+}
